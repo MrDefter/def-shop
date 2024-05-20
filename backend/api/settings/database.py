a5079296963a4ce3f61bed99f1dea5d28af18d0f
Artem Andreev <andreev.a@ct-sg.ru>
2024-05-20 09:54:52 +0300

    Дописаны настройки для базы данных.

--- backend/api/settings/database.py
+++ backend/api/settings/database.py
@@ -1,7 +1,10 @@
 """Настройки для postgres"""
 
+from functools import cache
+
 from pydantic import Field
 from pydantic_settings import BaseSettings
+from sqlalchemy.engine import URL
 
 
 class PostgresSettings(BaseSettings):
@@ -10,7 +13,7 @@ class PostgresSettings(BaseSettings):
         description='Хост для базы данных postgres.',
         examples=['localhost'],
     )
-    POSTGRES_PORT: str = Field(
+    POSTGRES_PORT: int = Field(
         description='Порт для базы данных postgres.',
         examples=[5432],
     ),
@@ -29,3 +32,32 @@ class PostgresSettings(BaseSettings):
 
     class Config:
         env_file = '.env'
+
+
+@cache
+def get_postgres_settings() -> PostgresSettings:
+    """Получить настройки для postgres.
+
+    Returns:
+        настройки для базы данных postgres.
+    """
+    return PostgresSettings()
+
+
+@cache
+def get_postgres_url() -> URL:
+    """Получить настройки url для postgres.
+
+    Returns:
+        Настройки url Для базы данных postgres.
+    """
+    settings = get_postgres_settings()
+    return URL(
+        drivername='postgresql',
+        database=settings.POSTGRES_DATABASE,
+        username=settings.POSTGRES_USER,
+        password=settings.POSTGRES_PASSWORD,
+        host=settings.POSTGRES_HOST,
+        port=settings.POSTGRES_PORT,
+        query={},
+    )
