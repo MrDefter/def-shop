607089de62f7b7b8a73707586af49423fb26e3e0
Artem Andreev <andreev.a@ct-sg.ru>
2024-05-20 10:46:55 +0300

    Исправлены настройки.

--- backend/settings/__init__.py
+++ backend/settings/__init__.py
@@ -1,4 +1,6 @@
 """Настройки."""
 
 from .templates_path import get_templates_path_settings
-from .postgres import get_postgres_settings
+from .database import get_postgres_settings, get_postgres_url
+
+__all__ = ['get_postgres_settings', 'get_postgres_url']
